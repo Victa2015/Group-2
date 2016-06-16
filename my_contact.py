26150029d8586c37f1aa807cf9ec9f651eca0d00
Desterio D <desteriodennis@live.com>
2016-06-16 16:59:02 +0300

    Do code clean-up

--- my_contact.py
+++ my_contact.py
@@ -15,19 +15,18 @@ class Contact(object):
         name = input("Name: ")
         phone = input("Number: ")
         self.numbers[name] = phone
-		
-	def remove_contact(self):
-		culprit = self.search()
-		if culprit in self.numbers:
-		 	del self.numbers[culprit]
-			return "{0} was deleted".format(culprit)
-		else:
-			return "{0} could not be deleted".format(culprit)		
-		
-		
-	def search(self):
-		name = input("Name:")
-		if name in self.numbers:
-			return self.numbers[name]
-		else:
-			return "{0} was not found \n".format(name)
+
+    def remove_contact(self):
+        culprit = self.search()
+        if culprit in self.numbers:
+            del self.numbers[culprit]
+            return "{0} was deleted".format(culprit)
+        else:
+            return "{0} could not be deleted".format(culprit)
+
+    def search(self):
+        name = input("Name:")
+        if name in self.numbers:
+            return self.numbers[name]
+        else:
+            return "{0} was not found \n".format(name)

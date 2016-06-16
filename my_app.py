8f6f953d68b9d8ccf681a54800fd54f261e57c28
Desterio D <desteriodennis@live.com>
2016-06-16 17:04:19 +0300

    Code the main app for running the app

--- my_app.py
+++ my_app.py
@@ -1,3 +1,6 @@
+from my_contact import Contact
+
+
 def print_menu():
     print('1. Print Phone Numbers')
     print('2. Add a Phone Number')
@@ -5,3 +8,22 @@ def print_menu():
     print('4. Lookup a Phone Number')
     print('5. Quit')
     print()
+
+
+menu_choice = 0
+print_menu()
+
+my_app = Contact()
+
+while menu_choice != 5:
+    menu_choice = int(input("Type in a number (1-5): "))
+    if menu_choice == 1:
+        my_app.list()
+    elif menu_choice == 2:
+        my_app.create()
+    elif menu_choice == 3:
+        my_app.remove_contact()
+    elif menu_choice == 4:
+        my_app.search()
+    elif menu_choice != 5:
+        print_menu()

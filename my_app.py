a6a2016006f53b47214eb80544f0605b0e576d68
Victor Kironde <victa2015@gmail.com>
2016-06-16 16:02:24 +0300

    Added the Menu Items for the Console UI

--- my_app.py
+++ my_app.py
@@ -1,1 +1,7 @@
-
+def print_menu():
+    print('1. Print Phone Numbers')
+    print('2. Add a Phone Number')
+    print('3. Remove a Phone Number')
+    print('4. Lookup a Phone Number')
+    print('5. Quit')
+    print()
